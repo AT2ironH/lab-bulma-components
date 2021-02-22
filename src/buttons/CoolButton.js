4cd535f06dccd4a8acbc2e96fbc26926f5112adb
Aleksandar <aleksandar.m.trifunovic@gmail.com>
2021-02-22 22:47:09 +0100

    lab done

--- src/buttons/CoolButton.js
+++ src/buttons/CoolButton.js
@@ -1,19 +1,23 @@
-import React from 'react'
-
-function CoolButtons (props){
-
-    // Conditionally render classes
-    // Note this is a hint for the lab if you're reading this.
-    // If you've understood how props work this would be easy peassy
-    let blue = props.className ? props.className : 'is-primary'
-    
-    return (
-      <div>
-        <div className={blue}>
-          <button>Primary</button>
-        </div>
-      </div>
-    );
-}
-
+import React from "react";
+const CoolButtons = (props) => {
+  console.log(props);
+  let classes = "button";
+  if (props.isPrimary) {
+    classes += " is-primary";
+  }
+  if (props.isDanger) {
+    classes += " is-danger";
+  }
+  if (props.isSuccess) {
+    classes += " is-success";
+  }
+  if (props.className) {
+    classes += " is-rounded my-class";
+  }
+  return (
+    <div className="field">
+      <button className={classes}>{props.children}</button>
+    </div>
+  );
+};
 export default CoolButtons;
